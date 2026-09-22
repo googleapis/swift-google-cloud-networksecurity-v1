4ea48638fd9cf19868aab0a5c8bd5fa8e2446b45
Carlos O'Ryan <coryan@google.com>
2026-09-21 20:01:37 -0400

    chore: update librarian with suppressed diagnostics (#1014)
    In this version of librarian all the 'DeprecatedDeclaration' warnings
    are supressed.
    
    We want these diagnostics because they inform customers when they are
    trying to use a service, method, message, field, enum or enum value that
    has been deprecated.
    
    We also need to use the same elements, if nothing else, to return them
    when the service includes them in a response, or allow customers to keep
    using them until they have time to fix their code.
    
    Until Swift 6.4 we could not supress the diagnostics, which was too
    noisy. With this PR the diagnostics are suppressed, and the CI system
    now can enable warnings for other locations (say examples) in case we
    use the deprecated elements where we shouldn't.
    
    On Swift 6.2 and 6.3, the diagnostics are not suppressed. Not ideal. On
    the other hand, we expect those to go away in 18 months or so.

--- Sources/GoogleCloudNetworkSecurityV1/InterceptEndpointGroupAssociation.swift
+++ Sources/GoogleCloudNetworkSecurityV1/InterceptEndpointGroupAssociation.swift
@@ -132,6 +132,9 @@ public struct InterceptEndpointGroupAssociation: Codable, Equatable, GoogleWKT._
     ]
   }
 
+  #if hasAttribute(diagnose)
+    @diagnose(DeprecatedDeclaration, as: ignored)
+  #endif
   public init(from decoder: Decoder) throws {
     let container = try decoder.container(keyedBy: CodingKeys.self)
     if let value = try container.decodeIfPresent(Swift.String.self, forKey: .name) {
@@ -175,6 +178,9 @@ public struct InterceptEndpointGroupAssociation: Codable, Equatable, GoogleWKT._
     }
   }
 
+  #if hasAttribute(diagnose)
+    @diagnose(DeprecatedDeclaration, as: ignored)
+  #endif
   public func encode(to encoder: Encoder) throws {
     var container = encoder.container(keyedBy: CodingKeys.self)
     try container.encode(self.name, forKey: .name)
--- Sources/GoogleCloudNetworkSecurityV1/MirroringEndpointGroupAssociation.swift
+++ Sources/GoogleCloudNetworkSecurityV1/MirroringEndpointGroupAssociation.swift
@@ -125,6 +125,9 @@ public struct MirroringEndpointGroupAssociation: Codable, Equatable, GoogleWKT._
     ]
   }
 
+  #if hasAttribute(diagnose)
+    @diagnose(DeprecatedDeclaration, as: ignored)
+  #endif
   public init(from decoder: Decoder) throws {
     let container = try decoder.container(keyedBy: CodingKeys.self)
     if let value = try container.decodeIfPresent(Swift.String.self, forKey: .name) {
@@ -165,6 +168,9 @@ public struct MirroringEndpointGroupAssociation: Codable, Equatable, GoogleWKT._
     }
   }
 
+  #if hasAttribute(diagnose)
+    @diagnose(DeprecatedDeclaration, as: ignored)
+  #endif
   public func encode(to encoder: Encoder) throws {
     var container = encoder.container(keyedBy: CodingKeys.self)
     try container.encode(self.name, forKey: .name)
